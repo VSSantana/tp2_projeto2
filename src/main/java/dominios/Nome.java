bd5beaadb561f728cfcfb59daf318227cea29628
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2018-10-16 09:28:20 -0300

    Inicio da codificação da classe Nome.

--- src/main/java/dominios/Nome.java
+++ src/main/java/dominios/Nome.java
@@ -1,4 +1,26 @@
 package dominios;
 
 public class Nome {
+
+    private String nome;
+    private int nomeSize = 100;
+
+    void SetNome (String nome) {
+
+        ValidarNome(nome);
+
+        this.nome = nome;
+
+    }
+
+    void ValidarNome (String nome) {
+
+        if (nome.length() > nomeSize || nome.isEmpty()) {
+
+            throw new IllegalArgumentException("Nome não deve ser nulo ou maior que 100 caracteres.");
+
+        }
+
+    }
+
 }
