be72709c1fd935cb0f741886457c0ff49a5e57ed
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2018-10-30 11:00:41 -0300

    Definição dos métodos da classe de domínio nome.

--- src/main/java/dominios/Nome.java
+++ src/main/java/dominios/Nome.java
@@ -1,9 +1,10 @@
 package dominios;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Nome {
 
     private String nome;
-    private int nomeSize = 100;
 
     public Nome () {}
 
@@ -17,9 +18,28 @@ public class Nome {
 
     private void validarNome (String nome) {
 
+        int nomeSize = 100;
         if (nome.length() > nomeSize || nome.isEmpty()) {
 
             throw new IllegalArgumentException("Nome não deve ser nulo ou maior que 100 caracteres.");
+
+        }
+
+        Pattern pattern = Pattern.compile("[\\d]");
+        Matcher matcher = pattern.matcher(nome);
+
+        if(matcher.find()){
+
+            throw new IllegalArgumentException("O nome não deve conter números.");
+
+        }
+
+        Pattern patternName = Pattern.compile("^((\\b[A-zÀ-ú']{2,40}\\b)\\s*){2,}$");
+        Matcher matcherName = patternName.matcher(nome);
+
+        if(!matcherName.matches()){
+
+            throw new IllegalArgumentException("O nome não deve conter símbolos.");
 
         }
 
--- src/main/java/testes/dominio/NomeTest.java
+++ src/main/java/testes/dominio/NomeTest.java
@@ -4,19 +4,36 @@ import dominios.Nome;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-
 class NomeTest {
 
     @org.junit.jupiter.api.Test
-    void setNome() {
+    void setNomeValido () {
 
         String nomePessoa = "Marcos Roberto Silveira Reis";
         Nome nome = new Nome();
 
         nome.setNome(nomePessoa);
 
-        assertEquals (nomePessoa.toUpperCase(), nome.getNome());
+        assertEquals(nomePessoa.toUpperCase(), nome.getNome());
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNomeInvalidoComNumeros () {
+
+        String nomePessoa = "Marcos Roberto Silveira Reis 1º";
+        Nome nome = new Nome();
+
+        try {
+
+            nome.setNome(nomePessoa);
+            fail("A execeção esperada não foi lançada.");
+
+        } catch (IllegalArgumentException exception) {
+
+            assertEquals("O nome não deve conter números.", exception.getMessage());
+
+        }
 
     }
 
@@ -26,7 +43,56 @@ class NomeTest {
         String nomePessoa = "Pedro de Alcântara João Carlos Leopoldo Salvador Bibiano Francisco Xavier de Paula Leocádio Miguel Gabriel Rafael Gonzaga";
         Nome nome = new Nome();
 
-        nome.setNome(nomePessoa);
+        try {
+
+            nome.setNome(nomePessoa);
+
+            fail("A exceção esperada não foi lançada.");
+
+        } catch (IllegalArgumentException exception) {
+
+            assertEquals("Nome não deve ser nulo ou maior que 100 caracteres.", exception.getMessage());
+
+        }
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNomeInvalidoNulo() throws IllegalArgumentException {
+
+        String nomePessoa = "";
+        Nome nome = new Nome();
+
+        try {
+
+            nome.setNome(nomePessoa);
+
+            fail("A exceção esperada não foi lançada.");
+
+        } catch (IllegalArgumentException exception) {
+
+            assertEquals("Nome não deve ser nulo ou maior que 100 caracteres.", exception.getMessage());
+
+        }
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNomeInvalidoComSimbolos () {
+
+        String nomePessoa = "M@rcos Roberto Silveira Reis";
+        Nome nome = new Nome();
+
+        try {
+
+            nome.setNome(nomePessoa);
+            fail("A execeção esperada não foi lançada.");
+
+        } catch (IllegalArgumentException exception) {
+
+            assertEquals("O nome não deve conter símbolos.", exception.getMessage());
+
+        }
 
     }
 
