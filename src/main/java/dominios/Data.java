42e4530e203a930e44bda6bcb4c47f55576816d0
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2018-11-01 09:45:48 -0300

    Implementação da classe Data e inicio de implementação da classe CPF.

--- src/main/java/dominios/Data.java
+++ src/main/java/dominios/Data.java
@@ -1,4 +1,19 @@
 package dominios;
 
+import java.util.Date;
+
 public class Data {
+
+    private Date data;
+
+    public Data () {}
+
+    public void setData (Date date){
+
+        this.data = data;
+
+    }
+
+    public Date getDate () { return data; }
+
 }
