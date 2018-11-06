f6525123a0dcbb2e29549a31a69322514eea121a
Vinícius de Sousa Santana <sousavinicius@hotmail.com>
2018-11-06 10:04:20 -0200

    Inclusão dos funcionários do setores 2 a 4 no banco de dados.

--- src/main/java/logica/AlterarEmpregado.java
+++ src/main/java/logica/AlterarEmpregado.java
@@ -1,4 +1,17 @@
 package logica;
 
+import java.sql.Connection;
+
 public class AlterarEmpregado {
+
+    private Connection connection;
+
+    public AlterarEmpregado (Connection connection) {
+
+        this.connection = connection;
+
+    }
+
+
+
 }
--- src/main/java/logica/CadastrarEmpregado.java
+++ src/main/java/logica/CadastrarEmpregado.java
@@ -1,4 +1,17 @@
 package logica;
 
+import java.sql.Connection;
+
 public class CadastrarEmpregado {
+
+    private Connection connection;
+
+    public CadastrarEmpregado (Connection connection) {
+
+        this.connection = connection;
+
+    }
+
+
+
 }
